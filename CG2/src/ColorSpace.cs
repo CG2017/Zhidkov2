943016e7d6432998ebf0355c549dfc6856b1f00a
Alexander <sasha3849@yandex.ru>
2017-03-06 01:09:05 +0300

    Lab 2 has been added.

---
+++ CG2/src/ColorSpace.cs
@@ -0,0 +1,38 @@
+﻿namespace ColorReplacer
+{
+    public delegate double ComparisonAlgorithm(IColorSpace a, IColorSpace b);
+
+    public interface IColorSpace
+    {
+        void Initialize(IRgb color);
+
+        IRgb ToRgb();
+
+        T To<T>() where T : IColorSpace, new();
+
+        double Compare(IColorSpace compareToValue, IColorSpaceComparison comparer);
+    }
+
+    public abstract class ColorSpace : IColorSpace
+    {
+        public abstract void Initialize(IRgb color);
+        public abstract IRgb ToRgb();
+        public double Compare(IColorSpace compareToValue, IColorSpaceComparison comparer)
+        {
+            return comparer.Compare(this, compareToValue);
+        }
+
+        public T To<T>() where T : IColorSpace, new()
+        {
+            if (typeof (T) == GetType())
+            {
+                return (T) MemberwiseClone();
+            }
+
+            var newColorSpace = new T();
+            newColorSpace.Initialize(ToRgb());
+
+            return newColorSpace;
+        }
+    }
+}
---
+++ CG2/src/ColorSpaces.cs
@@ -0,0 +1,91 @@
+﻿namespace ColorReplacer
+{
+    public interface IRgb : IColorSpace
+    {
+        double R { get; set; }
+
+        double G { get; set; }
+
+        double B { get; set; }
+    }
+
+    public class Rgb : ColorSpace, IRgb
+    {
+        public double R { get; set; }
+
+        public double G { get; set; }
+
+        public double B { get; set; }
+
+
+        public override void Initialize(IRgb color)
+        {
+            RgbConverter.ToColorSpace(color, this);
+        }
+
+        public override IRgb ToRgb()
+        {
+            return RgbConverter.ToColor(this);
+        }
+    }
+
+
+    public interface IXyz : IColorSpace
+    {
+        double X { get; set; }
+
+        double Y { get; set; }
+
+        double Z { get; set; }
+    }
+
+    public class Xyz : ColorSpace, IXyz
+    {
+        public double X { get; set; }
+
+        public double Y { get; set; }
+
+        public double Z { get; set; }
+
+
+        public override void Initialize(IRgb color)
+        {
+            XyzConverter.ToColorSpace(color, this);
+        }
+
+        public override IRgb ToRgb()
+        {
+            return XyzConverter.ToColor(this);
+        }
+    }
+
+    public interface ILab : IColorSpace
+    {
+        double L { get; set; }
+
+        double A { get; set; }
+
+        double B { get; set; }
+    }
+
+    public class Lab : ColorSpace, ILab
+    {
+        public double L { get; set; }
+
+        public double A { get; set; }
+
+        public double B { get; set; }
+
+
+        public override void Initialize(IRgb color)
+        {
+            LabConverter.ToColorSpace(color, this);
+        }
+
+        public override IRgb ToRgb()
+        {
+            return LabConverter.ToColor(this);
+        }
+    }
+
+}
